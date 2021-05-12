0b52c5a20359cb5cf8eb4d474af5e8d173665cc2
John <john@Serenity.local>
2021-05-12 18:19:24 -0400

    Added bottom tab navigation for IOS

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
+// gesture-handler must be at the top according to the documentation
+// for react-navigation
+// import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Decks from './components/Decks';
+import NewDeck from './components/NewDeck';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Decks" component={Decks} />
+        <Tab.Screen name="New Deck" component={NewDeck} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/NewDeck.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const NewDeck = () => {
+  return (
+    <Text>New Deck View</Text>
+  );
+}
+
+export default NewDeck;
